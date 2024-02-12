e7dea0d653e982a92fa51f087e1494797f9a3e50
jtagcat <git-12dbd862@jtag.cat>
2024-02-12 11:38:08 +0200

    patch

--- source/app/util.py
+++ source/app/util.py
@@ -805,6 +805,7 @@ def format_datetime(value, frmt):
 
 
 def hmac_sign(data):
+    return False
     key = bytes(app.config.get("IRIS", "SECRET_KEY"), "utf-8")
     h = hmac.HMAC(key, hashes.SHA256())
     h.update(data)
@@ -814,6 +815,7 @@ def hmac_sign(data):
 
 
 def hmac_verify(signature_enc, data):
+    return False
     signature = base64.b64decode(signature_enc)
     key = bytes(app.config.get("IRIS", "SECRET_KEY"), "utf-8")
     h = hmac.HMAC(key, hashes.SHA256())
